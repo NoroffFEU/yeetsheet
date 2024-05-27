f1583f28b04247e691be4975c195e32570e4c670
Krihag <125972171+Krihag@users.noreply.github.com>
2024-05-27 23:31:36 +0200

    added dataset values for col and row to be easily sellected

--- src/js/spreadsheet/cell.js
+++ src/js/spreadsheet/cell.js
@@ -1,8 +1,12 @@
 import createEle from '../helpers/createEle';
+import numberToLetter from '../helpers/numberToLetter';
 
 export default function cell(col, row) {
   const cellContainer = createEle('td', 'p-0  w-28  border relative');
-  cellContainer.setAttribute('id', col + '' + row);
+  cellContainer.setAttribute('id', numberToLetter(col) + (row + 1));
+
+  cellContainer.dataset.col = col;
+  cellContainer.dataset.row = row;
   // const cellInput = createEle('div', '  focus:bg-red-100');
 
   // cellContainer.append(cellInput);
