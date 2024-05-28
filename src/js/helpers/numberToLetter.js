7edc7524f86cd0eaffea1dab1e587ac1df2b2c31
Krihag <125972171+Krihag@users.noreply.github.com>
2024-05-28 11:52:22 +0200

    added usecase if needed more than 26 columns (up to 676) + js docs

--- src/js/helpers/numberToLetter.js
+++ src/js/helpers/numberToLetter.js
@@ -1,10 +1,19 @@
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 console.log(alphabet);
 
+/**
+ * Converts a number to letters in the alphabet (A-Z).
+ * @param {number} number - The number to be converted.
+ * @returns {string} The letter(s) representation of the number.
+ */
 export default function numberToLetter(number) {
   if (number < 26) {
     return alphabet[number];
   } else {
-    // Add logic here if we need to convert numbers greater than 26 to letters (since all letters will have been used, can add solution were a second letter is added forexample if this is needed).
+    // if number is greater than 26 it will return as 2 letters (starting with AA, AB, AC, etc.)
+    // if needed in future with more than 2 letters, then code needs to be modified (currently not needed)
+    const firstLetter = alphabet[Math.floor(number / 26) - 1];
+    const secondLetter = alphabet[number % 26];
+    return firstLetter + secondLetter;
   }
 }
