bc2e3144d289c8bb511e3416696c314ce7a0a0c6
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-05-29 13:54:58 +0200

    Implement the features JS cell targeting

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import spreadsheet from './spreadsheet';
 import userColsAndRows from './helpers/userColsAndRows';
 import toggleDarkMode from './darkModeToggle/toggleDarkMode.mjs';
+import { addCellTargetingEvents } from './spreadsheet/cellNavigation';
 
 const spreadsheetContainer = document.querySelector('#spreadsheetContainer');
 
@@ -9,3 +10,8 @@ const [cols, rows] = userColsAndRows();
 spreadsheetContainer.append(spreadsheet(cols, rows));
 
 toggleDarkMode();
+
+addCellTargetingEvents('#spreadsheetContainer table', (col, row, value) => {
+  // Here you can put the save function with params for cell contents.
+  console.log('save', col, row, value);
+});
