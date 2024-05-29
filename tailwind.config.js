31feed941445eaa1c76ab6e44f074d7ca38ffe65
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-05-29 20:26:52 +0200

    update tailwind config with colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js, mjs}'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: '0 0.75rem',
+        start: true,
+      },
+      colors: {
+        'ys-black': '#101216',
+        'ys-darkGrey': '#1F2124',
+        'ys-mediumGrey': '#939393',
+        'ys-lightGrey': '#4A4C4F',
+        'ys-whiteGrey': '#D9D9D9',
+        'ys-purple': '#915EFF',
+      },
+    },
   },
   plugins: [],
   darkMode: 'selector',
