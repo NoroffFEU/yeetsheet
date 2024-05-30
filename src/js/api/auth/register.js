eb2701eb0e23a6bfd954d72b94fcbe48aca14d90
petternikolai <petkri14832@stud.noroff.no>
2024-05-30 21:58:12 +0200

    Comments  out example code for login.js and register.js files

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -2,18 +2,18 @@
 
 // EXAMPLE CODE
 
-import { API_AUTH, API_BASE, API_REGISTER } from '../constants.js';
-import { authFetch } from '../fetch.js';
+// import { API_AUTH, API_BASE, API_REGISTER } from '../constants.js';
+// import { authFetch } from '../fetch.js';
 
-export async function register(name, email, password) {
-  const response = await authFetch(API_BASE + API_AUTH + API_REGISTER, {
-    method: 'POST',
-    body: JSON.stringify({ name, email, password }),
-  });
+// export async function register(name, email, password) {
+//   const response = await authFetch(API_BASE + API_AUTH + API_REGISTER, {
+//     method: 'POST',
+//     body: JSON.stringify({ name, email, password }),
+//   });
 
-  if (response.ok) {
-    return await response.json();
-  }
+//   if (response.ok) {
+//     return await response.json();
+//   }
 
-  throw new Error('Could not register the account');
-}
+//   throw new Error('Could not register the account');
+// }
