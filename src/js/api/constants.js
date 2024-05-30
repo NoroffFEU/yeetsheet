82b59380adc28a05e7dab6bfbdda0e4d0637fef1
petternikolai <petkri14832@stud.noroff.no>
2024-05-30 08:31:55 +0200

    Includes mockup JS functions that will be needed if this project is to be connected to an API

---
+++ src/js/api/constants.js
@@ -0,0 +1,11 @@
+export const API_BASE = /* BASE URL OF THE API */ 'https://linktoapi.example';
+
+/* Replace the placeholders with the actual values */
+
+export const API_AUTH = '/auth';
+export const API_REGISTER = '/register';
+export const API_LOGIN = '/login';
+export const API_KEY_URL = '/create-api-key';
+export const API_LISTINGS = '/auction/listings';
+export const API_KEY =
+  /* RETRIEVE YOUR API KEY FROM THE API AND STORE IT HERE */ 'YOUR_API_KEY';
