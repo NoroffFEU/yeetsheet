17c484f56a686e82c2f55be35100394c2876f2eb
Krihag <125972171+Krihag@users.noreply.github.com>
2024-06-04 21:37:45 +0200

    temporary changes for html and main.js
    had to comment some functionality out temporary in main.js. Will have to find solution for later

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
-import spreadsheet from './spreadsheet';
-import userColsAndRows from './helpers/userColsAndRows';
-import numberToLetter from './helpers/numberToLetter';
+// import numberToLetter from './helpers/numberToLetter';
 import toggleDarkMode from './darkModeToggle/toggleDarkMode.mjs';
-import { addCellTargetingEvents } from './spreadsheet/cellNavigation';
-import { initDB, saveCellValue, getCellValue } from './spreadsheet/db.js';
+// import { addCellTargetingEvents } from './spreadsheet/cellNavigation';
+import { initDB } from './spreadsheet/db.js';
 import showFileMenu from './header/file.mjs';
 import showZoomMenu from './header/zoom.mjs';
 import consoleBtnsActiveState from './console/consoleBtns.mjs';
+import Spreadsheet from './spreadsheet/Class/index.js';
+import codeEditor from './codeEditor/index.js';
 
 const spreadsheetContainer = document.querySelector('#spreadsheetContainer');
+
+const sheet = new Spreadsheet();
 
 // indexedDB
 initDB()
@@ -27,24 +29,32 @@ initDB()
     // DarkMode
     toggleDarkMode();
 
-    const [cols, rows] = userColsAndRows();
-
     // Create and append the spreadsheet to the container
-    spreadsheetContainer.append(spreadsheet(cols, rows));
+    const spreadSheet = sheet.displaySheet();
+    spreadsheetContainer.append(spreadSheet);
 
-    addCellTargetingEvents(
-      '#spreadsheetContainer table',
-      (col, row) => {
-        const cellId = numberToLetter(col) + (row + 1);
-        // read cell value from IndexedDB
-        return getCellValue(cellId).then((value) => value || '');
-      },
-      (col, row, value) => {
-        const cellId = numberToLetter(col) + (row + 1);
-        // save cell value to IndexedDB
-        saveCellValue(cellId, value);
-      },
-    );
+    document
+      .querySelector('#add-row-btn')
+      .addEventListener('click', sheet.addRow);
+    document
+      .querySelector('#add-col-btn')
+      .addEventListener('click', sheet.addCol);
+    codeEditor(sheet);
+    console.log(sheet);
+
+    // addCellTargetingEvents(
+    //   '#spreadsheetContainer table',
+    //   (col, row) => {
+    //     const cellId = numberToLetter(col) + (row + 1);
+    //     // read cell value from IndexedDB
+    //     return getCellValue(cellId).then((value) => value || '');
+    //   },
+    //   (col, row, value) => {
+    //     const cellId = numberToLetter(col) + (row + 1);
+    //     // save cell value to IndexedDB
+    //     saveCellValue(cellId, value);
+    //   },
+    // );
   })
   .catch((error) => {
     console.error('Failed to initialize IndexedDB:', error);
