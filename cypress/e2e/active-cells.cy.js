6806752458314a709b3494233256caf43390ffa0
Krista Kaza <krikaz50070@stud.noroff.no>
2024-06-05 21:55:38 +0200

    e2e testing for active cells

---
+++ cypress/e2e/active-cells.cy.js
@@ -0,0 +1,60 @@
+describe('Clear indicators for Active Cells and Selections', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('should click on a cell in the spreadsheet', () => {
+    cy.get('#spreadsheetContainer').scrollTo('topLeft');
+
+    cy.get('#A1').scrollIntoView();
+    cy.get('#A1').click();
+    cy.get('#A1').should('exist');
+  });
+
+  it('should drag to select a range of cells', () => {
+    cy.get('#spreadsheetContainer').scrollTo('topLeft');
+
+    cy.get('#A1').scrollIntoView();
+    cy.get('#A1').trigger('mousedown', { which: 1 });
+
+    cy.get('#spreadsheetContainer').scrollTo('bottomRight');
+
+    cy.get('#B2').scrollIntoView();
+    cy.get('#B2').should('be.visible');
+
+    cy.get('#B2').trigger('mousemove', { which: 1 });
+    cy.get('#B2').trigger('mouseup', { which: 1 });
+
+    cy.get('#A1').should('exist');
+    cy.get('#B2').should('exist');
+  });
+
+  it('should reset active cell and selection indicators after refresh', () => {
+    cy.get('#spreadsheetContainer').scrollTo('topLeft');
+
+    cy.get('#A1').scrollIntoView();
+    cy.get('#A1').click();
+    cy.get('#A1').should('exist');
+
+    cy.get('#A1').scrollIntoView();
+    cy.get('#A1').trigger('mousedown', { which: 1 });
+
+    cy.get('#spreadsheetContainer').scrollTo('bottomRight');
+
+    cy.get('#B2').scrollIntoView();
+    cy.get('#B2').should('be.visible');
+
+    cy.get('#B2').trigger('mousemove', { which: 1 });
+    cy.get('#B2').trigger('mouseup', { which: 1 });
+
+    cy.get('#A1').should('exist');
+    cy.get('#B2').should('exist');
+
+    cy.reload();
+
+    cy.get('#spreadsheetContainer').scrollTo('topLeft');
+    cy.get('#A1').should('exist');
+    cy.get('#spreadsheetContainer').scrollTo('bottomRight');
+    cy.get('#B2').should('exist');
+  });
+});
