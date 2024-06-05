c4a6d11e4743c68d1ac24fba07b6c1785ee688b5
Krihag <125972171+Krihag@users.noreply.github.com>
2024-06-05 16:32:07 +0200

    changed logic on createCells (classes) so that if its getting data input from indexedDB it wont create extra arrays

--- src/js/spreadsheet/Class/helpers/createCells.js
+++ src/js/spreadsheet/Class/helpers/createCells.js
@@ -2,22 +2,33 @@ import Cell from '../Cell';
 import numberToLetter from '../../../helpers/numberToLetter';
 
 export default function createCells(rows, cols, allCells = [], parent) {
-  for (let i = 0; i < rows; i++) {
-    allCells.push([]);
-    for (let j = 0; j < cols; j++) {
-      const data = allCells.find((cell) => cell.row === i && cell.col === j);
-      allCells[i].push(
-        new Cell(
-          data
-            ? data
-            : {
-                row: i,
-                col: j,
-                id: `${numberToLetter(j)}${i + 1}`,
-                parent: parent,
-              },
-        ),
-      );
+  console.log(allCells);
+  if (allCells.length > 0) {
+    console.log(allCells[0][0]);
+    return allCells.map((row, i) =>
+      row.map(
+        (cell, j) =>
+          new Cell({
+            ...cell,
+            row: i,
+            col: j,
+            parent: parent,
+          }),
+      ),
+    );
+  } else {
+    for (let i = 0; i < rows; i++) {
+      allCells.push([]);
+      for (let j = 0; j < cols; j++) {
+        allCells[i].push(
+          new Cell({
+            row: i,
+            col: j,
+            id: `${numberToLetter(j)}${i + 1}`,
+            parent: parent,
+          }),
+        );
+      }
     }
   }
   return allCells;
