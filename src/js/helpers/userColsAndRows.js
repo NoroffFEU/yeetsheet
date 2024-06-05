e73206742a7e0ff351fb8dcac4dbb39a6ab659d3
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-06-05 20:07:41 +0200

    Added JSDoc comments to undocumented functions: toggleDarkMode, showDropdownMenu, closeOnOutsideClick, cell, cellRow, initDB, saveCellValue, getCellValue, showLoader, hideLoader, showMessage, consoleBtnsActiveState

--- src/js/helpers/userColsAndRows.js
+++ src/js/helpers/userColsAndRows.js
@@ -1,3 +1,18 @@
+/**
+ * Calculates the number of cells that can fit in the screen based on the provided cell width and cell height.
+ *
+ * This function returns an array with two values: the number of columns and the number of rows that can fit on the screen.
+ * Default values for cell width and cell height are provided, but they can be overwritten by passing arguments.
+ *
+ * @function userColsAndRows
+ * @param {number} [height=window.innerHeight] - The height of the screen. Defaults to window.innerHeight if not provided.
+ * @param {number} [width=window.innerWidth] - The width of the screen. Defaults to window.innerWidth if not provided.
+ * @param {number} [cellwidth=112] - The width of each cell. Defaults to 112 if not provided.
+ * @param {number} [cellHeight=26] - The height of each cell. Defaults to 26 if not provided.
+ * @returns {Array<number>} - An array containing two numbers: the number of columns and the number of rows.
+ *
+ */
+
 // return number of cells that can fit in the screen based on cellwidth and cellheight.
 // Set default values for cellwidth and cellheight, but if adding props while calling it will overwrite the default values.
 // This function will return an array with two values: width and height.
--- src/js/spreadsheet/cellRow.js
+++ src/js/spreadsheet/cellRow.js
@@ -2,6 +2,19 @@ import createEle from '../helpers/createEle';
 import cell from './cell';
 import ifValidNumber from '../helpers/ifValidNumber';
 
+/**
+ * Creates a table row container with a specified number of cells.
+ *
+ * This function generates a `tr` element containing the specified number of cells.
+ * It also includes a header cell displaying the row number. The row and column
+ * indices are validated before the row is created.
+ *
+ * @function cellRow
+ * @param {number} cols - The number of cells in the row.
+ * @param {number} row - The row index.
+ * @returns {HTMLElement|null} - The created table row element, or null if validation fails.
+ */
+
 // creates a cell row container with cols number of cells
 export default function cellRow(cols, row) {
   // adds a row number to the row (since it starts at 0)
