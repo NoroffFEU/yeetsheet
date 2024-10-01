0a7741516b20f6c37640a3d690f7987e217994a1
karlwoien <karlwoien@gmail.com>
2024-10-01 15:42:17 +0200

    modifies code to make the cell ID unique pr sheet

--- src/js/spreadsheet/cell.js
+++ src/js/spreadsheet/cell.js
@@ -6,16 +6,16 @@ export default function cell(row, col) {
   const activeSheetId = document.querySelector(
     '.spreadsheet-content:not(.hidden)',
   ).id;
+
+  // Create unique cell ID by combining the sheet ID with the column and row (e.g., 'spreadsheet1-A1')
+  const cellId = `${activeSheetId}-${numberToLetter(col)}${row + 1}`;
+
   const cellContainer = createEle('td', 'p-0 w-28 border relative');
-  cellContainer.setAttribute(
-    'id',
-    `${activeSheetId}-${numberToLetter(col)}${row + 1}`,
-  );
+  cellContainer.setAttribute('id', cellId);
 
   cellContainer.dataset.col = col;
   cellContainer.dataset.row = row;
 
-  const cellId = numberToLetter(col) + (row + 1);
   getCellValue(cellId).then((value) => {
     if (value !== null) {
       cellContainer.textContent = value;
