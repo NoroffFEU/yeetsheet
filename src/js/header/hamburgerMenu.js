756c378eb9b81d4fdf46c60f49055e772382f4a4
Rena Hashimi <renhas54143@stud.noroff.no>
2024-10-03 18:00:52 +0200

    Added toggle functionality for file menu and hamburger menu
    - Implemented the `toggleHamburgerMenu` function to handle toggling the hamburger navigation and file menu visibility.
    - The hamburger button now toggles the visibility of the navigation menu.
    - The file button toggles the visibility of the file menu and adds/removes an active state.
    - Added logic to close the menus when clicking outside them.

--- src/js/header/hamburgerMenu.js
+++ src/js/header/hamburgerMenu.js
@@ -1,10 +1,35 @@
 export function toggleHamburgerMenu() {
   const hamburgerBtn = document.getElementById('nav-hamburger-btn');
+  const fileBtn = document.getElementById('fileBtn');
   const nav = document.querySelector('nav');
+  const fileMenu = document.getElementById('fileMenu');
 
   if (hamburgerBtn && nav) {
-    hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.addEventListener('click', (e) => {
       nav.classList.toggle('hidden');
+      e.stopPropagation();
+    });
+  }
+  if (fileBtn && fileMenu) {
+    // File button toggle
+    fileBtn.addEventListener('click', (e) => {
+      fileBtn.classList.toggle('active');
+      e.stopPropagation();
+    });
+
+    // Close menu when clicking outside of it
+    document.addEventListener('click', (event) => {
+      const isClickOutsideNav = !nav.contains(event.target);
+      const isClickOutsideBtn = !hamburgerBtn.contains(event.target);
+
+      if (
+        isClickOutsideNav &&
+        isClickOutsideBtn &&
+        !nav.classList.contains('hidden')
+      ) {
+        nav.classList.add('hidden');
+        fileBtn.classList.remove('active');
+      }
     });
   }
 }
