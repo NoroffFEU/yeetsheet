7e50a57150f659f1148a0eb2db137dabdcc303bc
Rena Hashimi <renhas54143@stud.noroff.no>
2024-10-03 23:33:00 +0200

    Update HTML IDs and simplify JavaScript for hamburger menu toggle
    - Changed ID of the mobile navigation div to "mobileNav" // new workflow index.html.
    - Updated JavaScript to streamline event handling for the hamburger menu and file menu.

--- src/js/header/hamburgerMenu.js
+++ src/js/header/hamburgerMenu.js
@@ -1,35 +1,45 @@
 export function toggleHamburgerMenu() {
   const hamburgerBtn = document.getElementById('nav-hamburger-btn');
-  const fileBtn = document.getElementById('fileBtn');
   const nav = document.querySelector('nav');
+  const mobileNav = document.getElementById('mobileNav');
+  const fileBtn = document.getElementById('fileBtn');
   const fileMenu = document.getElementById('fileMenu');
 
-  if (hamburgerBtn && nav) {
-    hamburgerBtn.addEventListener('click', (e) => {
-      nav.classList.toggle('hidden');
-      e.stopPropagation();
-    });
-  }
-  if (fileBtn && fileMenu) {
-    // File button toggle
+  // Function to close the nav and file menu
+  const closeNav = () => {
+    nav.classList.add('hidden');
+    if (fileMenu) fileMenu.classList.add('hidden');
+  };
+
+  // Toggle the nav on hamburger button click
+  hamburgerBtn.addEventListener('click', (event) => {
+    nav.classList.toggle('hidden');
+    event.stopPropagation();
+  });
+
+  if (fileBtn) {
+    // Toggle the file menu on file button click
     fileBtn.addEventListener('click', (e) => {
-      fileBtn.classList.toggle('active');
+      if (fileMenu) fileMenu.classList.toggle('hidden');
       e.stopPropagation();
     });
-
-    // Close menu when clicking outside of it
-    document.addEventListener('click', (event) => {
-      const isClickOutsideNav = !nav.contains(event.target);
-      const isClickOutsideBtn = !hamburgerBtn.contains(event.target);
-
-      if (
-        isClickOutsideNav &&
-        isClickOutsideBtn &&
-        !nav.classList.contains('hidden')
-      ) {
-        nav.classList.add('hidden');
-        fileBtn.classList.remove('active');
-      }
-    });
   }
+
+  // Close menus when clicking outside of them
+  window.addEventListener('click', (event) => {
+    if (
+      !nav.contains(event.target) &&
+      !hamburgerBtn.contains(event.target) &&
+      !mobileNav.contains(event.target)
+    ) {
+      closeNav();
+    }
+    if (
+      fileMenu &&
+      !fileMenu.contains(event.target) &&
+      !fileBtn.contains(event.target)
+    ) {
+      fileMenu.classList.add('hidden');
+    }
+  });
 }
