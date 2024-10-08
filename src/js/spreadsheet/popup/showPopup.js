58c110bfbd4ef614bdf8559ba115e664bd964cc1
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-10-08 08:16:38 +0200

    border active state fix

--- src/js/spreadsheet/popup/showPopup.js
+++ src/js/spreadsheet/popup/showPopup.js
@@ -1,16 +1,32 @@
 import { createPopup } from './createPopup';
 
+let lastActiveTd = null;
+
 export function showPopup(event) {
   event.preventDefault();
 
   const targetTd = event.target;
 
-  targetTd.classList.add('border', 'border-white');
+  if (targetTd.tagName !== 'TD') {
+    return;
+  }
+
+  if (lastActiveTd && lastActiveTd !== targetTd) {
+    lastActiveTd.classList.remove('dark:border-white');
+    lastActiveTd.classList.add('dark:border-ys-overlay-5');
+  }
+  console.log('target id: ', targetTd);
+
+  targetTd.classList.add('dark:border-white');
+  targetTd.classList.remove('dark:border-ys-overlay-5');
+
+  console.log('Target td classes', targetTd.className);
 
   let popup = document.getElementById('cell-popup');
   if (popup) {
     popup.remove();
   }
+  console.log('targetTd after popup exists: ', targetTd.className);
 
   popup = createPopup(targetTd);
 
@@ -20,4 +36,6 @@ export function showPopup(event) {
 
   popup.style.top = `${rect.top + window.scrollY + rect.height / 2 - popup.offsetHeight / 2}px`;
   popup.style.left = `${rect.left + window.scrollX + rect.width}px`;
+
+  lastActiveTd = targetTd;
 }
