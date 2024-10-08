a6f669187be980ec8d1667d8c63b51a62bb44864
Jonas Larsen <127976348+jonlar93@users.noreply.github.com>
2024-10-08 17:07:54 +0200

    add comment

--- src/js/codeEditor/runEditor.js
+++ src/js/codeEditor/runEditor.js
@@ -10,8 +10,10 @@ export function runEditor() {
 
       try {
         // Step 3: Evaluate the Code
+        console.log('code that runs trough eval();', code);
         const result = eval(code);
         // Step 4: Log the result to the console
+        console.log(result);
         return result;
       } catch (error) {
         // Step 5: Handle any errors during evaluation
