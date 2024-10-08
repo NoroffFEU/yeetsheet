0f0a5093f1336e40f46b3d11f9cfe8904be487a3
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-10-08 10:30:50 +0200

    color change

--- src/js/spreadsheet/popup/handleOutsideClicks.js
+++ src/js/spreadsheet/popup/handleOutsideClicks.js
@@ -2,7 +2,7 @@ export function handleOutsideClicks(popup, lastActiveTd) {
   function onClick() {
     if (popup) {
       popup.remove();
-      lastActiveTd.classList.remove('dark:border-white');
+      lastActiveTd.classList.remove('dark:border-ys-pink-500');
       lastActiveTd.classList.add('dark:border-ys-overlay-5');
       document.removeEventListener('click', onClick);
     }
