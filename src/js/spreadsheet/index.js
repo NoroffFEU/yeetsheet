251c06c55c0fb9063c22c30c80af80f76e65c64e
Kinenystuen <kine_on@hotmail.com>
2024-10-10 11:09:03 +0200

    Added renderColumns functions

--- src/js/spreadsheet/index.js
+++ src/js/spreadsheet/index.js
@@ -1,17 +1,17 @@
 import createEle from '../helpers/createEle';
 import cellRow from './cellRow';
-import numberToLetter from '../helpers/numberToLetter';
 import ifValidNumber from '../helpers/ifValidNumber';
+
+// import { newColumn } from './newColumn';
+import { renderColumns } from './renderColumns';
 import {
   createContextMenuColumn,
   addContextMenuListener,
 } from '../helpers/columnRowMenu';
 
 // Create the context menus
-const { contextMenu, optionRight, optionLeft, optionDelete } =
-  createContextMenuColumn();
+const { contextMenu } = createContextMenuColumn();
 addContextMenuListener(contextMenu);
-
 /**
  * Creates a spreadsheet with the specified number of columns and rows.
  *
@@ -30,44 +30,7 @@ export default function spreadsheet(cols, rows) {
 
   columnNumbers.appendChild(emptyTh);
 
-  for (let i = 0; i < cols; i++) {
-    const colNumber = createEle(
-      'th',
-      'w-28 text-center border-x dark:border-ys-overlay-5 border-ys-amethyst-400 dark:bg-ys-overlay-15 bg-white py-2 snap-start',
-      numberToLetter(i),
-    );
-
-    // Add menu event listener to column header (A, B, C...)
-    colNumber.addEventListener('contextmenu', (e) => {
-      e.preventDefault();
-
-      const contextMenuRow = document.getElementById('contextMenuRow');
-      if (contextMenuRow) {
-        contextMenuRow.classList.add('hidden');
-      }
-
-      contextMenu.style.left = `${e.pageX}px`;
-      contextMenu.style.top = `${e.pageY}px`;
-      contextMenu.classList.remove('hidden');
-
-      optionRight.onclick = () => {
-        console.log(`Insert a column to the right of column ${i + 1}`);
-        contextMenu.classList.add('hidden');
-      };
-
-      optionLeft.onclick = () => {
-        console.log(`Insert a column to the left of column ${i + 1}`);
-        contextMenu.classList.add('hidden');
-      };
-
-      optionDelete.onclick = () => {
-        console.log(`Delete column ${i + 1}`);
-        contextMenu.classList.add('hidden');
-      };
-    });
-
-    columnNumbers.appendChild(colNumber);
-  }
+  renderColumns(cols, columnNumbers, tableBody);
 
   // Create table body rows
   for (let i = 0; i < rows; i++) {
