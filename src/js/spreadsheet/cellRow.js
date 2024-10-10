6745a135c1b4dd50d5be3761edf686352f35012e
Kinenystuen <kine_on@hotmail.com>
2024-10-10 15:46:03 +0200

    Added call renderRows function

--- src/js/spreadsheet/cellRow.js
+++ src/js/spreadsheet/cellRow.js
@@ -1,16 +1,7 @@
 import createEle from '../helpers/createEle';
 import cell from './cell';
 import ifValidNumber from '../helpers/ifValidNumber';
-import {
-  createContextMenuRow,
-  addContextMenuListener,
-} from '../helpers/columnRowMenu';
-import { highlightRow } from './cellHighlight';
-
-// Create the context menu globally
-const { contextMenuRow, optionAbove, optionBelow, optionDeleteRow } =
-  createContextMenuRow();
-addContextMenuListener(contextMenuRow);
+import { renderRows } from './renderRows';
 
 /**
  * Creates a table row container with a specified number of cells.
@@ -29,52 +20,10 @@ addContextMenuListener(contextMenuRow);
 export default function cellRow(cols, row) {
   // adds a row number to the row (since it starts at 0)
   const rowIndex = row + 1;
-
   if (!ifValidNumber(cols, rowIndex)) return;
-
   const rowContainer = createEle('tr', `row-${row} flex`);
 
-  // Displaying the row number
-  const rowNumber = createEle(
-    'th',
-    'w-28 text-center border-y dark:border-ys-overlay-5 border-ys-amethyst-400 dark:bg-ys-overlay-15 bg-white py-2 flex-none snap-start',
-    rowIndex,
-  );
-  highlightRow(rowNumber);
-
-  // Add menu event listener for the row header
-  rowNumber.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-
-    const contextMenuColumn = document.getElementById('contextMenuColumn');
-    if (contextMenuColumn) {
-      contextMenuColumn.classList.add('hidden');
-    }
-
-    contextMenuRow.style.left = `${e.pageX}px`;
-    contextMenuRow.style.top = `${e.pageY}px`;
-    contextMenuRow.classList.remove('hidden');
-
-    // Option for adding a row above
-    optionAbove.onclick = () => {
-      console.log(`Insert a row above row ${rowIndex}`);
-      contextMenuRow.classList.add('hidden');
-    };
-
-    // Option for adding a row below
-    optionBelow.onclick = () => {
-      console.log(`Insert a row below row ${rowIndex}`);
-      contextMenuRow.classList.add('hidden');
-    };
-
-    // Option for deleting the row
-    optionDeleteRow.onclick = () => {
-      console.log(`Delete row ${rowIndex}`);
-      contextMenuRow.classList.add('hidden');
-    };
-  });
-
-  rowContainer.appendChild(rowNumber);
+  renderRows(rowIndex, rowContainer, row, cols);
 
   for (let i = 0; i < cols; i++) {
     rowContainer.appendChild(cell(row, i));
