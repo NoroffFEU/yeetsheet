1fc4e057fe0db692f2f4258daac6f018820a772c
robfil50219 <robfil50219@stud.noroff.no>
2024-10-11 12:47:17 +0200

    Move dark mode toggle button into hamburger menu for small screens

--- src/js/darkModeToggle/toggleDarkMode.mjs
+++ src/js/darkModeToggle/toggleDarkMode.mjs
@@ -2,33 +2,40 @@
  * Toggles the dark mode for the website.
  *
  * This function checks the current theme from local storage and sets the initial state of dark mode accordingly.
- * It then adds a click event listener to the button with the ID "darkModeToggleBtn" to toggle the dark mode class on the
- * document's root element. The button's text content and the local storage theme value are updated based on the mode.
+ * It then adds a click event listener to both dark mode buttons for small and large screens.
+ * The button's text content and the local storage theme value are updated based on the mode.
  *
  * @function toggleDarkMode
  * @returns {void}
  */
 export default function toggleDarkMode() {
-  const darkModeToggleBtn = document.querySelector("#darkModeToggleBtn");
-  const darkModeToggleText = document.querySelector(".toggle-text");
-  if (darkModeToggleBtn) {
-    const theme = localStorage.getItem("theme");
-    if (theme === "light") {
+  const darkModeToggleBtns = document.querySelectorAll("#darkModeToggleBtn, #darkModeToggleBtnMobile");
+  const darkModeToggleText = document.querySelectorAll(".toggle-text");
+
+  function setDarkMode(isDarkMode) {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+      darkModeToggleText.forEach(text => text.innerText = "Dark on");
+      darkModeToggleBtns.forEach(btn => btn.classList.remove("light"));
+      localStorage.removeItem("theme");
+    } else {
       document.documentElement.classList.remove("dark");
-      darkModeToggleText.innerText = "Dark off"
-      darkModeToggleBtn.classList.add("light");
+      darkModeToggleText.forEach(text => text.innerText = "Dark off");
+      darkModeToggleBtns.forEach(btn => btn.classList.add("light"));
+      localStorage.setItem("theme", "light");
     }
-    darkModeToggleBtn.addEventListener("click", (e) => {
-      document.documentElement.classList.toggle("dark");
-      if (!document.documentElement.classList.contains("dark")) {
-        e.target.classList.add("light");
-        darkModeToggleText.innerText = "Dark off"
-        localStorage.setItem("theme", "light")
-      } else {
-        e.target.classList.remove("light");
-        darkModeToggleText.innerText = "Dark on"
-        localStorage.removeItem("theme")
-      }
-    });
   }
+
+  // Set initial state based on local storage
+  const theme = localStorage.getItem("theme");
+  setDarkMode(theme !== "light");
+
+  // Add event listeners for both buttons
+  darkModeToggleBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const isDarkMode = !document.documentElement.classList.contains("dark");
+      setDarkMode(isDarkMode);
+    });
+  });
 }
+
